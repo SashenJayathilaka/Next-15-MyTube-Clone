13959414caf599be42764a607abe3a236276d286
Sashen Hasindu <sashenhaindu@gmail.com>
2025-02-11 11:46:12 +1100

    📹 Studio videos

--- app/(studio)/studio/page.tsx
+++ app/(studio)/studio/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { DEFAULT_LIMIT } from "@/constants";
+import StudioView from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-function Page() {
-  return <div>Page</div>;
+async function Page() {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
 
 export default Page;
+
+// 06.07
---
+++ modules/studio/ui/view/studio-view.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoSections from "../sections/video-sections";
+
+function StudioView() {
+  return (
+    <div>
+      <VideoSections />
+    </div>
+  );
+}
+
+export default StudioView;
