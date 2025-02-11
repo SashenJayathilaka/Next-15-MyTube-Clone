2167b013fb932909060b05a32dbed3fd63dafdc1
Sashen Hasindu <sashenhaindu@gmail.com>
2025-02-11 22:34:23 +1100

    🔃 Infinite loading\

---
+++ hooks/use-intersetions-observer.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useIntersectionsObserver = (
+  options?: IntersectionObserverInit
+) => {
+  const [isIntersection, setIsIntersection] = useState(false);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersection(entry.isIntersecting);
+    }, options);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [options]);
+
+  return { targetRef, isIntersection };
+};
--- modules/studio/ui/sections/video-sections.tsx
+++ modules/studio/ui/sections/video-sections.tsx
@@ -1,10 +1,32 @@
 "use client";
 
+import InfiniteScroll from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import Link from "next/link";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-function VideoSections() {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+export const videosSections = () => {
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideoSectionsSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+function VideoSectionsSuspense() {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -13,7 +35,52 @@ function VideoSections() {
     }
   );
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>vis</TableCell>
+                    <TableCell>vis</TableCell>
+                    <TableCell>vis</TableCell>
+                    <TableCell>vis</TableCell>
+                    <TableCell>vis</TableCell>
+                    <TableCell>vis</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
 }
 
-export default VideoSections;
+export default VideoSectionsSuspense;
